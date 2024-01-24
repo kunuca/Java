482fc7d37f6762795216239d9dc3665fef043782
Kustova Tatiana <kunuca@mail.ru>
2024-01-25 00:34:59 +0400

    02

--- src/Les1/Main1.java
+++ src/Les1/Main1.java
@@ -1,7 +1,31 @@
 package Les1;
 
+//В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида
+//        "Доброе утро, <Имя>!", если время от 05:00 до 11:59
+//        "Добрый день, <Имя>!", если время от 12:00 до 17:59;
+//        "Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+//        "Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+
+import java.util.Scanner;
+import java.time.LocalTime;
+
 public class Main1 {
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Представьтесь пожалуйста");
+        String name=scanner.nextLine();
+        LocalTime time=LocalTime.now();
+        int hour=time.getHour();
+        if (hour>=5 && hour<12) {
+            System.out.println("Доброе утро, " + name + "!");
+//            System.out.printf("Доброе утро, %s!", name);
+        } else if (hour>=12 && hour<18){
+                System.out.println("Добрый день, " + name+"!");}
+        else if (hour>=18 && hour<23) {
+            System.out.println("Добрый вечер, "+name+ "!");
+            }
+        else System.out.println("Доброй ночи, "+name+"!");
+        }
     }
-}
+    31:36
+
